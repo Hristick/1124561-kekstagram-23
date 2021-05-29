2638bbb23c4be77b96b1d44d6e51d2c8ddc34892
Hristick <hristick@mail.ru>
2021-05-29 14:57:57 +0300

    Добавил функции проверки длины коммента и функцию получения рандомного числа из диапазона

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // Получает положительное рандомное целое число > или = 0
+const ERROR = '';
+
 function getRandomIntInclusive(min, max) {
   if ((min >= 0) && (min < max)){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {const ERROR = 'Введите правильный диапазон чисел!!!' ;}
+  } else {ERROR = 'Введите правильный диапазон чисел!!!' ;}
 }
 getRandomIntInclusive();
 
