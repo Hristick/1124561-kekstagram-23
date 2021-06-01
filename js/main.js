be93b1d87d2d89414f58aba5ea51db7eaadafe52
Hristick <hristick@mail.ru>
2021-06-01 15:10:52 +0300

    Исправил замечания V9.1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Получает положительное рандомное целое число > или = 0
-const getRandomInt = (min, max) => ((min >= 0) && (min < max)) ? Math.floor(Math.random() * (max - min + 1)) + min : 0;
+const getRandomInt = (min = 0, max = 0) => ((min >= 0) && (min < max)) ? Math.floor(Math.random() * (max - min + 1)) + min : 0;
 
 //  Проверяет длину коментария
-const checkCommentLength = (str, maxCommentLength) => str.length <= maxCommentLength;
+const checkCommentLength = (str = '', maxCommentLength = 140) => str.length <= maxCommentLength;
 
 getRandomInt();
 checkCommentLength();
