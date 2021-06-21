42b09f23725c88c7cde7116d82398d6468ac68f0
Hristick <hristick@mail.ru>
2021-06-21 21:52:49 +0300

    module1-task1

---
+++ js/util.js
@@ -0,0 +1,15 @@
+// Получает положительное рандомное целое число > или = 0
+const getRandomInt = (min = 0, max = 0) => ((min >= 0) && (min < max)) ? Math.floor(Math.random() * (max - min + 1)) + min : 0;
+
+//  Проверяет длину коментария
+const checkCommentLength = (str = '', maxCommentLength = 140) => str.length <= maxCommentLength;
+
+const getUnquiArray = (count = 0) => {
+  const result = new Set();
+  while (result.size < count) {
+    result.add(getRandomInt(1,300));
+  }
+  return Array.from(result);
+};
+
+export {getRandomInt, checkCommentLength, getUnquiArray};
