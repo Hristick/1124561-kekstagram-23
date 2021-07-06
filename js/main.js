882d2dd83ef8e0f202cbb5aacbaf2851dbdad2b3
Hristick <hristick@mail.ru>
2021-07-06 09:13:47 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ import { MAX_POST, getArrayMocks } from './render-moks-data.js';
 import { renderPhotos } from './render-photos.js';
 
 const moksPostsArray = getArrayMocks(MAX_POST);
-
 const renderedPhoto = renderPhotos(moksPostsArray);
 document.querySelector('.pictures').appendChild(renderedPhoto);
