35296304931b2c67240028a6bd3a436e180925fd
Hristick <hristick@mail.ru>
2021-07-13 09:14:34 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 import { getArrayMocks } from './render-moks-data.js';
 import { renderPhotos } from './render-photos.js';
 
+const appendPhotos = document.querySelector('.pictures');
 const MAX_POST = 25;
 const moksPostsArray = getArrayMocks(MAX_POST);
 const renderedPhoto = renderPhotos(moksPostsArray);
-
-const appendPhotos = document.querySelector('.pictures');
 appendPhotos.appendChild(renderedPhoto);
-console.log(renderedPhoto);
+
--- js/render-photos.js
+++ js/render-photos.js
@@ -1,6 +1,7 @@
 const pictureTemplate = document.querySelector('#picture').content;
 const copyPicture = pictureTemplate.querySelector('.picture');
 
+
 const renderPhotos = (array = []) => {
   const result = document.createDocumentFragment();
   for (let index = 0; index < array.length; index++) {
@@ -8,8 +9,10 @@ const renderPhotos = (array = []) => {
     renderedPicture.querySelector('.picture__img').src = array[index].url;
     renderedPicture.querySelector('.picture__likes').textContent = array[index].likes;
     renderedPicture.querySelector('.picture__comments').textContent = array[index].comments.length;
+    result.appendChild(renderedPicture);
   }
   return result;
 };
+
 
 export{renderPhotos};
