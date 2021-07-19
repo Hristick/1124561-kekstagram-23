3062d5744f98e6077fad6f464b4bc9112ec51b67
Hristick <hristick@mail.ru>
2021-07-19 21:42:53 +0300

    module8-task2

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,6 +1,7 @@
 const upload = document.querySelector('.img-upload__overlay');
 const uploadCancel = document.querySelector('#upload-cancel');
 const upLoadFile = document.querySelector('#upload-file');
+const textHashtags = document.querySelector('.text__hashtags');
 
 const openPopup = () => {
   upload.classList.remove('hidden');
@@ -15,7 +16,22 @@ const closePopup = () => {
 const onRenderPopup = () => {
   upLoadFile.addEventListener('change', openPopup());
   uploadCancel.addEventListener('click', closePopup);
-  window.addEventListener('keydown', )
 };
+
+const userNameInput = document.querySelector('.setup-user-name');
+
+textHashtags.addEventListener('input', () => {
+  const valueLength = textHashtags.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    textHashtags.setCustomValidity(`Ещё ${  MIN_NAME_LENGTH - valueLength } симв.`);
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    textHashtags.setCustomValidity(`Удалите лишние ${  valueLength - MAX_NAME_LENGTH } симв.`);
+  } else {
+    textHashtags.setCustomValidity('');
+  }
+
+  textHashtags.reportValidity();
+});
 
 export { onRenderPopup };
